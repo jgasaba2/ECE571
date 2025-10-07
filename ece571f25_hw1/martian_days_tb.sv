6e74bf2070b80798b3bac89d99840961a6a4056c
Janvier Mpfizi Rutihunza <92325891+jgasaba2@users.noreply.github.com>
2025-10-07 14:28:30 -0700

    Update martian_days_tb.sv

--- ece571f25_hw1/martian_days_tb.sv
+++ ece571f25_hw1/martian_days_tb.sv
@@ -8,20 +8,21 @@ module martian_days_tb;
     // DUT (keep name/ports as in your project)
     martian_days DUT (.M(M), .LY(LY), .D27(D27), .D28(D28));
 
-    initial begin
-        integer m; // loop variable for month
-        // simple waveform-oriented sweep: months 0..23, LY=0 then LY=1
-       for int (m = 0; m < 24; m = m + 1) begin
-            M = m;
-            LY = 0; #10;
-            $display("Month=%0d, LY=%0d => D27=%0d, D28=%0d", M, LY, D27, D28);
+initial begin
+    integer m; // loop variable for month
+    // simple waveform-oriented sweep: months 0..23, LY=0 then LY=1
+    for (m = 0; m < 24; m = m + 1) begin
+        M = m;
+        LY = 0; #10;
+        $display("Month=%0d, LY=%0d => D27=%0d, D28=%0d", M, LY, D27, D28);
 
-            LY = 1; #10;
-            $display("Month=%0d, LY=%0d => D27=%0d, D28=%0d", M, LY, D27, D28);
-        end
-        $finish;
+        LY = 1; #10;
+        $display("Month=%0d, LY=%0d => D27=%0d, D28=%0d", M, LY, D27, D28);
     end
+    $finish;
+end
 endmodule
+
 
 
 
